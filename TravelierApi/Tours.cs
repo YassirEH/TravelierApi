48df3e0e543a20c9a657927e9600a33cf248c618
YassirElHalouti <elhaloutiyassir@gmail.com>
2023-12-08 15:11:32 +0100

    Add project files.

---
+++ TravelierApi/Tours.cs
@@ -0,0 +1,13 @@
+﻿namespace TravelierApi
+{
+    public class Tours
+    {
+        public int Id { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public string Image { get; set; }
+        public string Duration { get; set; }
+        public string Price { get; set; }
+        public string Country { get; set; }
+    }
+}
