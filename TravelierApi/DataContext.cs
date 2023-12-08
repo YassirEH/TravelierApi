a14a6cfe150ae29c9aae3eb2c8491969d2fe084f
YassirElHalouti <elhaloutiyassir@gmail.com>
2023-12-08 15:18:36 +0100

    added DataContext, installed EF(ye forgot to do it earlier)

---
+++ TravelierApi/DataContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TravelierApi
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<Tours> Tours { get; set; }
+    }
+}
