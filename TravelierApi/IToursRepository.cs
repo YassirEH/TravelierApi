9dd79a4f800836667c945d7d1f1fef2d6f585c07
YassirElHalouti <elhaloutiyassir@gmail.com>
2023-12-08 15:20:23 +0100

    added Tours Repository Interface

---
+++ TravelierApi/IToursRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace TravelierApi
+{
+    public interface IToursRepository
+    {
+        IEnumerable<Tours> GetAllTours();
+        Tours GetToursById(int id);
+        void AddTours(Tours tours);
+        void UpdateTours(Tours tours);
+        void DeleteTours(int id);
+        void Save();
+    }
+}
